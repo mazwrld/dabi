6074cf3f2a67de19647f5d4bb4055b54c975e31a
Maz <mazspm@icloud.com>
2023-05-07 00:03:32 -0500

    finished all sever endpoints

--- server/main.go
+++ server/main.go
@@ -29,7 +29,31 @@ func main() {
 			return err
 		}
 
-		return c.JSON(todo)
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+	})
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+
+		for index, todo := range todos {
+			if todo.ID == id {
+				todos[index].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
